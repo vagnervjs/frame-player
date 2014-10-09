7e3ec2a1c0c6a6ba7958a49770c63ef009f6a467
Jonathan Cousins <j.cousins@ry.com>
2014-10-09 15:33:44 +0100

    More robut argument checks & position variety
    - The app now checks if enough arguments have been given.
    
    - If the first argument given is not a number (i.e. not a frame
      number), the frameStart and frameEnd variables are set by the
      files in the given folder.
    
    - The image file type extension is no longer hard coded as `.png`.
      This was because ffmpeg can also output jpgs.

--- src/converter/app.js
+++ src/converter/app.js
@@ -8,29 +8,53 @@ function base64Img(src) {
     return util.format('data:%s;base64,%s', mime.lookup(src), data);
 }
 
-var frameStart = process.argv[2],
-    frameEnd = process.argv[3],
-    folder = process.argv[4],
-    outputFile = process.argv[5],
-    dataUri = null,
-    framesImg = [];
+/*
+ *    Possible argument positions
+ *
+ *    converter frameStart(number) frameEnd(number) folder(string) outputFile(string)
+ *    converter folder(string) outputFile(string)
+ *
+ */
 
-for (; frameStart <= frameEnd; frameStart++) {
-    console.log('Converting file: ' + frameStart);
-    dataUri = base64Img(folder + frameStart + '.png');
-    framesImg[frameStart] = dataUri;
-}
-
-framesImg.splice(null, 1);
-
-var json = {
-    frames: framesImg
-};
-
-fs.writeFile(outputFile, JSON.stringify(json), function(err) {
-    if(err) {
-        console.log(err);
+if(process.argv.length > 2) {
+    // Check if the first argument is a number or string
+    if(!isNaN(parseInt(process.argv[2]))) {
+        var frameStart = process.argv[2],
+            frameEnd = process.argv[3],
+            folder = process.argv[4],
+            outputFile = process.argv[5],
+            dataUri = null,
+            framesImg = [];
     } else {
-        console.log('Completed!');
+        var frameStart = 1,
+            frameEnd = fs.readdirSync(process.argv[2]).length,
+            folder = process.argv[2],
+            outputFile = process.argv[3];
     }
-});
+
+    var dataUri = null,
+        framesImg = [],
+        fileTypeExt = path.extname(fs.readdirSync(folder)[0]); //If there's a better way, please do it...
+
+    for (; frameStart <= frameEnd; frameStart++) {
+        console.log('Converting file: ' + frameStart);
+        dataUri = base64Img(folder + frameStart + fileTypeExt);
+        framesImg[frameStart] = dataUri;
+    }
+
+    framesImg.splice(null, 1);
+
+    var json = {
+        frames: framesImg
+    };
+
+    fs.writeFile(outputFile, JSON.stringify(json), function(err) {
+        if(err) {
+            console.log(err);
+        } else {
+            console.log('Completed!');
+        }
+    });
+} else {
+    console.error('Not enough parameters supplied!');
+}
